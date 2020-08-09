ec05e0b966dc4c7a627eba02b36af31b1e5c1f77
Christina Deming <christinamdeming@gmail.com>
2020-08-09 11:22:12 -0400

    We're talking content, style, & additional script

---
+++ public/witch.js
@@ -0,0 +1,10 @@
+function hide() {
+    document.querySelector("#cookie").setAttribute("class", "fade-out");
+    document.querySelector("#1").setAttribute("class","fade-in");
+}
+
+function next(id) {
+    let i = id.parentNode.id;
+    document.querySelector(`#${i}`).setAttribute("class", "fade-out");
+    document.querySelector(`#${i+1}`).setAttribute("class", "fade-in");
+}
